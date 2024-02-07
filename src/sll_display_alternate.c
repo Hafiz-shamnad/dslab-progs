d8d130a475539dc0e1d2c164c4db16457ad935e6
coder <coder@coder>
2024-02-07 17:15:09 +0000

    Add the following sll_display_alternate.c sll_sum_and_avg.c sll_sum_of_even.c

---
+++ src/sll_display_alternate.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* link;
+};
+
+struct Node* head = NULL;
+
+void insert(int value) {
+    struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+    new->data = value;
+    new->link = head;
+    head = new;
+}
+
+void display() {
+    struct Node* ptr = head;
+    if (head == NULL) {
+        printf("List is empty\n");
+    } else {
+        while (ptr != NULL) {
+            printf("%d ", ptr->data);
+            ptr = ptr->link;
+        }
+        printf("\n");
+    }
+}
+
+void display_alt() {
+    struct Node* ptr = head;
+    if (head == NULL) {
+        printf("List is empty\n");
+    } else {
+        while (ptr != NULL) {
+            printf("%d ", ptr->data);
+            if (ptr->link == NULL) break;
+            ptr = ptr->link->link;
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int choice, value;
+
+    printf("SLL operations\n");
+    printf(" [1] Insert\n");
+    printf(" [2] Display normal\n");
+    printf(" [3] Display alternate\n");
+    printf(" [4] Exit\n");
+    while (1) {
+        printf("\nEnter a choice: ");
+        scanf("%d", &choice);
+        if (choice == 1) {
+            printf("Enter value to insert: ");
+            scanf("%d", &value);
+            insert(value);
+        } else if (choice == 2) {
+            display();
+        } else if (choice == 3) {
+            display_alt();
+        } else if (choice == 4) {
+            break;
+        } else {
+            printf("INVALID CHOICE\n");
+        }
+    }
+    return 0;
+}
